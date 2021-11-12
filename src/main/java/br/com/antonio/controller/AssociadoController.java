8029f791cd7d8822431ca39f9d5fa6948503497c
Antonio Eloi Fernandes Junior <antonio.junior@spread.com.br>
2021-11-12 18:32:27 -0300

    adicionando swagger

--- src/main/java/br/com/antonio/controller/AssociadoController.java
+++ src/main/java/br/com/antonio/controller/AssociadoController.java
@@ -19,6 +19,7 @@ import br.com.antonio.domain.Associado;
 import br.com.antonio.domain.request.AssociadoRequest;
 import br.com.antonio.domain.response.AssociadoPost;
 import br.com.antonio.service.AssociadoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -31,24 +32,28 @@ public class AssociadoController {
 	private final AssociadoService associadoService;
 	
 	@GetMapping
+	@Operation(summary = "Lista todos os associados", tags = {"associado"})
 	public ResponseEntity<List<Associado>> listarTodos() {
 		log.info("Listagem de associados: {}");
 		return ResponseEntity.ok(associadoService.listarTodos());
 	}
 	
 	@GetMapping(path = "/{nome}")
+	@Operation(summary = "Lista todos os associados por nome", tags = {"associado"})
 	public ResponseEntity<List<Associado>> listarPorNome(@PathVariable String nome) {
 		log.info("Listagem por nome: {}");
 		return ResponseEntity.ok(associadoService.listarPorNome(nome));
 	}
 	
 	@PostMapping
+	@Operation(summary = "Salva um associado", tags = {"associado"})
 	public ResponseEntity<Associado> salvar(@RequestBody @Valid AssociadoPost associadoPost) {
 		log.info("Salvar associado: {}");
 		return new ResponseEntity<>(associadoService.salvar(associadoPost),HttpStatus.CREATED);
 	}
 	
 	@PutMapping
+	@Operation(summary = "Atualiza um associado", tags = {"associado"})
 	public ResponseEntity<Void> atualizar(@RequestBody AssociadoRequest associadoRequest) {
 		log.info("Atualizar associado: {}");
 		associadoService.atualizar(associadoRequest);
@@ -56,6 +61,7 @@ public class AssociadoController {
 	}
 	
 	@DeleteMapping(path = "/{id}")
+	@Operation(summary = "Deleta um associado", tags = {"associado"})
 	public ResponseEntity<Void> excluir(@PathVariable long id) {
 		log.info("Excluir associado: {}");
 		associadoService.excluir(id);
